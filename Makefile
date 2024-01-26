a3797d1c3e8898b9de2d3c7fbce4d7b9fd82b4c4
Alif Al Hasan <48673852+alifalhasan@users.noreply.github.com>
2024-01-26 17:26:08 +0600

    Merged develop into main

---
+++ Makefile
@@ -0,0 +1,13 @@
+PROJECT = "ML Project Init"
+AUTHOR = "Alif Al Hasan"
+RELEASE = "0.0.1"
+
+.SILENT:
+.ONESHELL:
+
+docs:
+	echo "Generating docs..." && \
+	cd docs && \
+	sphinx-quickstart -q -p ${PROJECT} -a ${AUTHOR} -r ${RELEASE} --ext-viewcode --ext-todo --ext-autodoc
+
+.PHONY: docs
---
+++ src/pipes/test.py
@@ -0,0 +1,21 @@
+def add(x, y):
+    """
+    Performs addition.
+
+    Parameters:
+    - x: The first input number.
+    - y: The second input number.
+
+    Returns:
+    int: The resulting answer after addition.
+
+    Example:
+    >>> x = 2
+    >>> y = 3
+    >>> add(x, y)
+    5
+    """
+    return (x + y)
+
+if __name__ == "__main__" :
+    print(add(2, 3))
